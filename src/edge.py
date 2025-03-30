11d33516930fce59323f767154de21904071c2a4
Omer Melih Gul <omermelihgul@hotmail.com>
2025-03-30 17:06:46 -0400

    general improvements

--- src/edge.py
+++ src/edge.py
@@ -1,14 +1,36 @@
 import sys
 import requests
 from utils import *
+from flask import Flask, request
+
+app = Flask(__name__)
 
 port = port()
 local = local(port)
+
 edge_index = sys.argv[1]
-client_index = sys.argv[2]
+# client_index = sys.argv[2]
 server = sys.argv[3]
 
+clients = set()
+
+# register edge
 url = f'http://{server}/register?address={local}&role=edge&edge_index={edge_index}'
 res = requests.get(url)
-print(res)
-print("edge")
+
+@app.route('/')
+def home():
+    return 'OK'
+
+@app.route('/bind')
+def bind():
+    address = request.args.get('address', None)
+    clients.add(address)
+
+    url = f'http://{address}/bind?address={local}'
+    requests.get(url)
+
+    return 'OK'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port, debug=False)
--- src/server.py
+++ src/server.py
@@ -1,5 +1,7 @@
+import time
 import config
 import socket
+import requests
 import subprocess
 from utils import *
 from flask import Flask, request
@@ -14,9 +16,9 @@ edges = dict()
 clients = dict()
 num_of_edges = len(config.data['edges'])
 num_of_clients = sum(len(edge['clients']) for edge in config.data['edges'])
-time = config.data.get('defaults', {}).get('time', '00:05:00')
+t_time = config.data.get('defaults', {}).get('time', '00:05:00')
 
-print(f"Time: {time}")
+print(f"Time: {t_time}")
 print(f"Number of edges: {num_of_edges}")
 print(f"Total number of clients: {num_of_clients}\n")
 
@@ -27,7 +29,7 @@ for edge_index, edge in enumerate(config.data['edges']):
     cpu = edge.get('cpu', 1)
     cluster = edge.get('cluster', 'intel18')
 
-    jobs.add(submit(time, mem, cpu, cluster, local, 'edge', edge_index))
+    jobs.add(submit(t_time, mem, cpu, cluster, local, 'edge', edge_index))
     print(f"Edge {edge_index} - Mem: {mem}, CPU: {cpu}, Cluster: {cluster}")
 
     for client_index, client in enumerate(edge['clients']):
@@ -35,7 +37,7 @@ for edge_index, edge in enumerate(config.data['edges']):
         cpu = client.get('cpu', 1)
         cluster = client.get('cluster', 'intel18')
 
-        jobs.add(submit(time, mem, cpu, cluster, local, 'client', edge_index, client_index))
+        jobs.add(submit(t_time, mem, cpu, cluster, local, 'client', edge_index, client_index))
         print(f"  Client {client_index} - Mem: {mem}, CPU: {cpu}, Cluster: {cluster}")
 
 print(local)
@@ -51,24 +53,27 @@ def register():
     edge_index = request.args.get('edge_index', None)
     address = request.args.get('address', None)
 
-    print(role)
-    print(edge_index)
-    print(address)
     if role == 'edge':
         edges[address] = edge_index
     else:
         clients[address] = edge_index
 
     if len(edges) == num_of_edges and len(clients) == num_of_clients:
+        time.sleep(5)
+        for edge_address, edge_id in edges.items():
+            for client_address, client_id in clients.items():
+                if edge_id == client_id:
+                    url = f'http://{edge_address}/bind?address={client_address}'
+                    res = requests.get(url)
         print("Ready!")
 
     return 'OK'
 
 @app.route('/kill')
 def kill():
-    ids = ' '.join(map(str, jobs))
-    subprocess.run(['sbatch', ids])
-    return f"Successfully canceled jobs: {ids}"
+    for id in jobs:
+        subprocess.run(['scancel', id])
+    return f"Successfully canceled jobs"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port, debug=False)

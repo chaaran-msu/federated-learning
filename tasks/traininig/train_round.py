57779e1ceaa06563634736f9334ab10cd4f849fd
chaaran757 <47863104+chaaran757@users.noreply.github.com>
2025-04-06 13:33:33 -0400

    Added code for training rounds of each type

--- tasks/traininig/train_round.py
+++ tasks/traininig/train_round.py
@@ -8,14 +8,19 @@ sys.path.append(os.path.join(dirname, '../'))
 import torch
 from torch import nn
 from typing import List
+import requests
 
 from traininig.data.create_dataloaders import create_dataloaders_flower
 from traininig.models.baseline import Baseline
 from traininig.utils.parameters import load_serialized_parameters, get_serialized_parameters
 from traininig.utils.train import train
 from traininig.utils.test import test
+from aggregation.aggregate import aggregate
 
-def train_round(
+# Initialize model
+model = Baseline()
+
+def train_model(
     num_clients: int,
     partition_id: int,
     batch_size: int,
@@ -29,9 +34,6 @@ def train_round(
         partition_id=partition_id,
         batch_size=batch_size
     )
-
-    # Initialize model
-    model = Baseline()
 
     # Load model parameters
     load_serialized_parameters(
@@ -80,3 +82,70 @@ def train_round(
     }
 
     return round_data
+
+def train_round_client(
+    id: str,
+    server_address: str,
+    num_clients: int,
+    partition_id: int,
+    batch_size: int,
+    parameters: List,
+    learning_rate: float,
+    num_epochs: int
+):
+    # Train the model
+    round_data = train_model(
+        num_clients,
+        partition_id,
+        batch_size,
+        parameters,
+        learning_rate,
+        num_epochs
+    )
+
+    # Send model to server for aggregation
+    round_data['id'] = id
+
+    # Send data back to server
+    requests.post(
+        url=f'{server_address}/aggregate',
+        json=round_data
+    )
+
+def train_round_edge(
+    current_round_data,
+    server_address,
+    device_id
+):
+    # Aggregate parameters from all clients
+    total_num_samples, aggregated_parameters = aggregate(current_round_data)
+
+    data = {
+        'id': device_id,
+        'num_samples': total_num_samples,
+        'parameters': aggregated_parameters
+    }
+
+    # Send parameters up the hierarchy        
+    requests.post(f"{server_address}/aggregate", json=data)
+
+def train_round_server(
+    round_data,
+    round_clients
+):
+    # Aggregate Parameters
+    total_num_samples, aggregated_parameters = aggregate(round_data)
+
+    # Start Training for next round
+    for client in round_clients:
+        client_address = client['address']
+
+        data = {
+            'batch_size': 16,
+            'learning_rate': 0.1,
+            'num_epochs': 1,
+            'parameters': aggregated_parameters
+        }
+
+        # Ask the client to start training
+        requests.post(f"{client_address}/start_training", json=data)

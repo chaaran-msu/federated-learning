ce367a706cd9c772b94b3b35d7f301b13d590c2b
chaaran757 <47863104+chaaran757@users.noreply.github.com>
2025-04-10 13:36:56 -0400

    Added code for multi-levl hfl

---
+++ systems/multi_level_fl/allocate_resources.py
@@ -0,0 +1,152 @@
+import os
+import sys
+
+dirname = os.path.abspath(os.path.dirname(__file__))
+
+sys.path.append(os.path.join(dirname))
+
+import uuid
+
+from roles.utils import submit
+
+architecture = {
+    'defaults': {'time': '00:05:00'},
+    'edges': [
+        {
+            'mem': 1,
+            'cpu': 1,
+            'cluster': 'intel18',
+            'edges': [
+                {
+                    'mem': 1,
+                    'cpu': 1,
+                    'cluster': 'intel18',
+                    'clients': [
+                        {'mem': 1, 'cpu': 1, 'cluster': 'intel18'},
+                        {'mem': 1, 'cpu': 1, 'cluster': 'intel18'}
+                    ]
+                },
+                {
+                    'mem': 1,
+                    'cpu': 1,
+                    'cluster': 'intel18',
+                    'clients': [
+                        {'mem': 1, 'cpu': 1, 'cluster': 'intel18'},
+                        {'mem': 1, 'cpu': 1, 'cluster': 'intel18'}
+                    ]
+                },
+            ]  
+        },
+        {
+            'mem': 1,
+            'cpu': 1,
+            'cluster': 'intel18',
+            'edges': [
+                {
+                    'mem': 1,
+                    'cpu': 1,
+                    'cluster': 'intel18',
+                    'clients': [
+                        {'mem': 1, 'cpu': 1, 'cluster': 'intel18'},
+                        {'mem': 1, 'cpu': 1, 'cluster': 'intel18'}
+                    ]
+                },
+                {
+                    'mem': 1,
+                    'cpu': 1,
+                    'cluster': 'intel18',
+                    'clients': [
+                        {'mem': 1, 'cpu': 1, 'cluster': 'intel18'},
+                        {'mem': 1, 'cpu': 1, 'cluster': 'intel18'}
+                    ]
+                },
+            ]
+        }
+    ]
+}
+
+def allocate_resources(
+    main_server_id,
+    main_server_address,
+    architecture_name
+):
+    # Submit jobs to allocate resources
+    jobs = set()
+
+    num_edges = len(architecture['edges'])
+    num_clients = sum(len(edge['clients']) for edge in architecture['edges'])
+    t_time = architecture.get('defaults', {}).get('time', '00:05:00')
+
+    for edge_index_fl, edge_fl in enumerate(architecture['edges']):
+        edge_id_fl = uuid.uuid4()
+
+        mem = edge_fl.get('mem', 1)
+        cpu = edge_fl.get('cpu', 1)
+        cluster = edge_fl.get('cluster', 'intel18')
+
+        jobs.add(
+            submit(
+                device_id=edge_id_fl,
+                server_id=main_server_id,
+                main_server_address=main_server_address,
+                role='edge',
+                time=t_time, 
+                mem=mem, 
+                cpu=cpu, 
+                cluster=cluster,
+                architecture_name=architecture_name
+            )
+        )
+        print(f"Job submitted for edge first level {edge_index_fl+1}")
+
+        for edge_index_sl, edge_sl in enumerate(edge_fl['edges']):
+            edge_id_sl = uuid.uuid4()
+
+            mem = edge_sl.get('mem', 1)
+            cpu = edge_sl.get('cpu', 1)
+            cluster = edge_sl.get('cluster', 'intel18')
+
+            jobs.add(
+                submit(
+                    device_id=edge_id_sl,
+                    server_id=edge_id_fl,
+                    main_server_address=main_server_address,
+                    role='edge',
+                    time=t_time, 
+                    mem=mem, 
+                    cpu=cpu, 
+                    cluster=cluster,
+                    architecture_name=architecture_name
+                )
+            )
+            print(f"Job submitted for edge second level {edge_index_sl+1}")
+
+            for client_index, client in enumerate(edge_sl['clients']):
+                client_id = uuid.uuid4()
+
+                mem = client.get('mem', 1)
+                cpu = client.get('cpu', 1)
+                cluster = client.get('cluster', 'intel18')
+
+                jobs.add(
+                    submit(
+                        device_id=client_id,
+                        server_id=edge_id_sl,
+                        main_server_address=main_server_address,
+                        role='client',
+                        time=t_time, 
+                        mem=mem, 
+                        cpu=cpu, 
+                        cluster=cluster,
+                        architecture_name=architecture_name
+                    )
+                )
+
+                print(f"Job submitted for client {client_index+1}")
+
+    num_devices = {
+        'edges': num_edges,
+        'clients': num_clients
+    }
+
+    return jobs, num_devices
---
+++ systems/multi_level_fl/registration.py
@@ -0,0 +1,160 @@
+import os
+import sys
+
+dirname = os.path.abspath(os.path.dirname(__file__))
+
+sys.path.append(os.path.join(dirname))
+sys.path.append(os.path.join(dirname, '../../'))
+
+import requests
+from collections import defaultdict
+
+from tasks.traininig.start_training import start_training_server
+
+def inform_server(
+    server_address,
+    client_id,
+    client_address
+):
+    # Construct the URL to inform the edge about its client
+    edge_url = f'http://{server_address}/register_client'
+
+    # Send the POST request to the edge to register the client
+    try:
+        edge_res = requests.post(
+            edge_url,
+            json={
+                'id': client_id,
+                'address': client_address
+            }
+        )
+
+        # Check if the request was successful
+        if edge_res.status_code == 200:
+            print(f"Successfully bound client {client_address} to edge {server_address}")
+        else:
+            print(f"Failed to bind client {client_address} to edge {server_address}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error while trying to bind client {client_address} to edge {server_address}: {e}")    
+
+
+def inform_client(
+    client_address,
+    server_id,
+    server_address,
+    partition_id,
+    num_clients
+):
+    client_url = f'http://{client_address}/register_server'
+
+    # Send the POST request to the client to inform it of its assigned edge
+    try:
+        client_res = requests.post(
+            client_url,
+            json={
+                'server_id': server_id,
+                'server_address': server_address,
+                'partition_id': partition_id,
+                'num_clients': num_clients
+            }
+        )
+        # Check if the request was successful
+        if client_res.status_code == 200:
+            print(f"Successfully informed client {client_address} about edge {server_address}")
+        else:
+            print(f"Failed to inform client {client_address} about edge {server_address}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error while trying to inform client {client_address} about edge {server_address}: {e}")
+
+def registration(
+    main_server_id,
+    main_server_address,
+    edges, 
+    clients,
+    current_round_clients
+):
+    partition_ids = defaultdict(list)
+
+    for client_idx, client_id in enumerate(clients):
+        client_address = clients[client_id]['address']
+        client_server_id = clients[client_id]['server_id']
+
+        if client_server_id == main_server_id:
+            server_address = main_server_address
+
+            # No need to inform the server about the client
+
+            # Inform the client about the server
+            inform_client(
+                client_address,
+                client_server_id,
+                server_address,
+                client_idx,
+                len(clients)
+            )
+        else:
+            server_address = edges[client_server_id]['address']
+
+            # Inform server about client
+            inform_server(
+                server_address,
+                client_id,
+                client_address
+            )
+
+            # Inform the client about the server
+            inform_client(
+                client_address,
+                client_server_id,
+                server_address,
+                client_idx,
+                len(clients)
+            )
+
+        # Add the partition ID to the edge server it is connected to
+        partition_ids[client_server_id].append(client_idx)
+    
+    # Inform the edges about their server
+    for edge_id in edges:
+        edge_address = edges[edge_id]['address']
+        server_id = edges[edge_id]['server_id']
+
+        # Inform the edge server about their server
+        if server_id == main_server_id:
+            server_address = main_server_address
+        else:
+            server_address = edges[server_id]['address']
+
+        # Construct the URL to inform the edge about its server
+        edge_url = f'http://{edge_address}/register_server'
+
+        # Send the POST request to the edge to register the server
+        try:
+            edge_res = requests.post(
+                edge_url,
+                json={
+                    'id': server_id,
+                    'address': server_address,
+                    'partition_ids': [0], # TODO: Correct this later
+                    'num_clients': len(clients)
+                }
+            )
+
+            # Check if the request was successful
+            if edge_res.status_code == 200:
+                print(f"Successfully bound edge {edge_address} to server {main_server_address}")
+            else:
+                print(f"Failed to bind edge {edge_address} to server {main_server_address}")
+        except requests.exceptions.RequestException as e:
+            print(f"Error while trying to bind edge {edge_address} to server {main_server_address}: {e}")
+
+    print("Connections have been made and ready for training")
+
+    # Start first round of training
+    # Parameters will be initialized randomly
+    start_training_server(
+        clients=current_round_clients,
+        first_round=True
+    )
+
+    print('Started first round of training')

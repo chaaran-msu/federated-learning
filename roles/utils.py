18e2a5930d1ef5982d0f8c5b183c9e4be61d4ded
chaaran757 <47863104+chaaran757@users.noreply.github.com>
2025-04-30 18:52:00 -0400

    Changed folder

--- roles/utils.py
+++ roles/utils.py
@@ -32,7 +32,7 @@ def submit(
 
 source ~/.venv/fl/bin/activate
 
-srun python {'~/federated_learning/roles/client/app.py' if role == 'client' else '~/federated_learning/roles/edge/app.py'} {device_id} {server_id} {main_server_address} {architecture} {num_edge_client_rounds}"""
+srun python {'~/federated-learning/roles/client/app.py' if role == 'client' else '~/federated-learning/roles/edge/app.py'} {device_id} {server_id} {main_server_address} {architecture} {num_edge_client_rounds}"""
 
     filename = f"{device_id}.sh"
     with open(filename, 'w') as file:
